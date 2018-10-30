d01b12d77104cf21a72d5354a6206dda33453949
Dion Lao <dion.lao@uqconnect.edu.au>
2018-10-30 15:51:15 +0800

    Changed name, learning syntax

--- wannadexWebApp/src/App.vue
+++ wannadexWebApp/src/App.vue
@@ -3,6 +3,7 @@
     <img src="./assets/logo.png">
     <router-view/>
   </div>
+
 </template>
 
 <script>
--- wannadexWebApp/src/components/HelloWorld.vue
+++ wannadexWebApp/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>WannaDex</h2>
     <ul>
       <li>
         <a
@@ -45,41 +45,7 @@
         </a>
       </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    
   </div>
 </template>
 
@@ -88,7 +54,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Wannabyte Studios'
     }
   }
 }
